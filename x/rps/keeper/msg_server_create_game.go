5819f8b6866a0d8a7122ef242e29b0ef657e469d
Ilya <groshev.i.d@gmail.com>
2023-04-19 02:23:09 +0200

    transfer tokes to module

--- x/rps/keeper/msg_server_create_game.go
+++ x/rps/keeper/msg_server_create_game.go
@@ -25,6 +25,14 @@ func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (
 		Turn1:     "",
 		Turn2:     "",
 	}
+	sender, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		panic(err)
+	}
+	sdkError := k.bankKeeper.SendCoinsFromAccountToModule(ctx, sender, types.ModuleName, sdk.Coins{msg.BetAmount})
+	if sdkError != nil {
+		return nil, sdkError
+	}
 	k.SetGames(ctx, game)
 	info.NextId++
 	k.SetSystemInfo(ctx, info)
